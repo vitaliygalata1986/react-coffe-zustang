231a029736d948ba0634fe5762682ddecb154ca5
vitaliy <nertis44@gmail.com>
2025-02-16 18:20:08 +0200

    Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import './App.css';
+import { useCounterStore } from './model/counterStore'; // кастомный Zustand-хук из файла ./model/counterStore
 
 function App() {
-  return <></>;
+  // const counter = useCounterStore((state) => state.counter);
+  const { counter, increment, decrement } = useCounterStore(); // Вытаскиваем counter из Zustand-хранилища
+  return (
+    <>
+      <button onClick={increment}>+</button>
+      <span>{counter}</span>
+      <button onClick={decrement}>-</button>
+    </>
+  );
 }
 
 export default App;
---
+++ src/model/counterStore.ts
@@ -0,0 +1,40 @@
+import { create, StateCreator } from 'zustand';
+
+/*
+	create — основная функция из zustand, которая создает хранилище (store).
+	StateCreator — это тип для функции, создающей slice (кусок состояния).
+*/
+
+// Определение типа состояния
+type CounterState = {
+  counter: number; // Описываем CounterState — объект с одним числовым полем counter
+};
+
+type CounterActions = {
+  increment: () => void;
+  decrement: () => void;
+};
+
+// Создание slice (части состояния)
+// Это функция, которая определяет начальное состояние: counter: 0 — начальное значение счетчика.
+const counterSlice: StateCreator<CounterState & CounterActions> = (
+  set,
+  get
+) => ({
+  counter: 0,
+  decrement() {
+    const { counter } = get(); // получаем с помощью get() значение counter из состояния
+    // set((state) => ({ ...state, counter: counter - 1 }));
+    // но если не нужно менять весь state
+    set({ counter: counter - 1 });
+  },
+  increment() {
+    const { counter } = get();
+    set({ counter: counter + 1 });
+  },
+});
+
+// Создание хранилища Zustand
+export const useCounterStore = create<CounterState & CounterActions>(counterSlice);
+
+// create<CounterState>(counterSlice) создает Zustand-хук, который можно использовать в компонентах React
